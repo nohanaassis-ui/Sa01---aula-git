da79eaf31a1d4840440abb23b67425a9079e601a
nohanaassis-ui <nohana.assis@edu.senai.br>
2026-03-25 15:13:56 -0300

    Feat(ex08):acesso ao evento

---
+++ src/EX08.java
@@ -0,0 +1,20 @@
+void main() {
+
+    int idadepermitida = 18;
+
+    Scanner sc = new Scanner(System.in);
+    IO.println("Bem vindo ao ínicio do LOWBAPALOOZA");
+    IO.println("");
+
+    IO.println("Qual é a sua idade ?");
+
+    int idade = sc.nextInt();
+
+    if (idade >= idadepermitida) {
+        IO.println("Seu acesso foi permitido! Você pode participar do evento.");
+
+    }else{
+        IO.println("Infelizmente seu acesso foi negado. (Menor de idade )");
+
+    }
+}
