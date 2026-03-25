6b333ae4bb8b9c7b0e4cd91006c5c8bff590757e
nohanaassis-ui <nohana.assis@edu.senai.br>
2026-03-25 16:53:43 -0300

    Feat(ex12):Progresso do curso

---
+++ src/EX12.java
@@ -0,0 +1,27 @@
+void main() {
+
+    Scanner sc = new Scanner(System.in);
+
+    IO.println("Bem vindo ao ProRso");
+    IO.println("");
+    IO.println("Qual o total de aulas no seu curso? (Ex: 150");
+
+    int totalaulas = sc.nextInt();
+    IO.println("Quantas aulas você assitiu");
+    int realizadas = sc.nextInt();
+
+    double porcentagem = (realizadas * 100.0 / totalaulas);
+
+    String resultado = String.format("%.1f", porcentagem);
+    IO.println("Progresso do curso = " + resultado + " %concluído. ");
+
+    if(porcentagem <50) {
+        IO.println("Você  ainda está no começo do curso ");
+
+    } else if (porcentagem <100) {
+        IO.println("Você está bem avançado ");
+
+    }else{
+        IO.println("Parabéns ! Curso aplicado ");
+    }
+}
